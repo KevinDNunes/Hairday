b47312cbbd1745082fdeaaf289d257df7bff3eae
Kevin Dambrosio Nunes <kevindnunes@gmail.com>
2026-01-04 21:38:52 -0300

    Terminando de configurar os horários

---
+++ src/modules/form/date-change.js
@@ -0,0 +1,9 @@
+import { schedulesDay } from "./schedules/load.js"
+
+// Seleciona o input de data.
+const selectedDate = document.getElementById("date")
+
+// Recarrega a lista de horários quando o input de datas mudar.
+selectedDate.onchange = () => schedulesDay()
+
+
--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -1,10 +1,14 @@
 import dayjs from "dayjs"
 
 import { openingHours } from "../../utils/opening-hours.js"
+import { hoursClick } from "./hours-click.js"
 
 const hours = document.getElementById("hours")
 
 export function hoursLoad({ date }) {
+    // Limpa a lista de horários.
+    hours.innerHTML = ""
+
     const opening = openingHours.map((hour) => {
         // Recupera somente a hora.
         const [scheduleHour] = hour.split(":")
@@ -14,7 +18,7 @@ export function hoursLoad({ date }) {
 
         return {
             hour,
-            avaliable: isHourPast,
+            available: isHourPast,
         }
     })
 
@@ -26,7 +30,26 @@ export function hoursLoad({ date }) {
         li.classList.add(available ? "hour-available" : "hour-unavailable")
 
         li.textContent = hour
+
+        if(hour === "09:00"){
+            hourHeaderAdd("Manhã")
+        } else if (hour === "13:00"){
+            hourHeaderAdd("Tarde")
+        } else if (hour === "18:00"){
+            hourHeaderAdd("Noite")
+        }
+
         hours.append(li)
     })
 
+    // Adiciona o evento de click nos horários disponíveis.
+    hoursClick()
+}
+
+function hourHeaderAdd(title) {
+    const header = document.createElement("li")
+    header.classList.add("hour-period")
+    header.textContent = title
+
+    hours.append(header)
 }
